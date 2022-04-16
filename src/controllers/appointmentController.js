698afd9e12d34c6a35f4852c319a6d6a284e6b70
Lucas Santos <lucas.santos@acensjr.com>
2022-04-16 08:07:22 -0300

    final adjustments

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -1,6 +1,3 @@
-
-import Joi from "joi";
-
 //Workaround to make prisma work with "type": "module"
 import prismaClient from "../prisma.js";
 
@@ -8,7 +5,6 @@ class AppointmentController {
 
     async store (req, res){
         const {name, birthdate, appointmentDate} = req.body;
-        const date = new Date(appointmentDate);
 
         const Appointment = await prismaClient.Appointments.create({
             data: {
@@ -20,11 +16,11 @@ class AppointmentController {
         return res.json(Appointment);
     }
 
-    async index (req, res){
+    /* async index (req, res){
         const registries = await prismaClient.Appointments.findMany();
 
         return res.json(registries);
-    }
+    } */
 }
 
 export default AppointmentController;
